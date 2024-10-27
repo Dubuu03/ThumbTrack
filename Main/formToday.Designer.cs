7dd44a23381ed39d2b1c46c5bb88e7347f77012e
Drix <narcisodrix@gmail.com>
2024-10-27 17:18:01 +0800

    removed time setting on formToday

--- Main/formToday.Designer.cs
+++ Main/formToday.Designer.cs
@@ -32,27 +32,6 @@
             this.lbDate = new System.Windows.Forms.Label();
             this.lbTime = new System.Windows.Forms.Label();
             this.timerTime = new System.Windows.Forms.Timer(this.components);
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtAmInEnd = new System.Windows.Forms.TextBox();
-            this.txtAmInStart = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.txtPmInEnd = new System.Windows.Forms.TextBox();
-            this.txtPmInStart = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.txtAmOutEnd = new System.Windows.Forms.TextBox();
-            this.txtAmOutStart = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.txtPmOutEnd = new System.Windows.Forms.TextBox();
-            this.txtPmOutStart = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.btnSubmit = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnExportAttendace = new System.Windows.Forms.Button();
@@ -61,10 +40,6 @@
             this.label10 = new System.Windows.Forms.Label();
             this.dgvAttendance = new System.Windows.Forms.DataGridView();
             this.label11 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            this.groupBox4.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAttendance)).BeginInit();
@@ -96,206 +71,6 @@
             // 
             this.timerTime.Interval = 1;
             this.timerTime.Tick += new System.EventHandler(this.timerTime_Tick);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.groupBox1.Controls.Add(this.txtAmInEnd);
-            this.groupBox1.Controls.Add(this.txtAmInStart);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(133, 492);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(260, 184);
-            this.groupBox1.TabIndex = 6;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Time In AM";
-            // 
-            // txtAmInEnd
-            // 
-            this.txtAmInEnd.Location = new System.Drawing.Point(45, 137);
-            this.txtAmInEnd.Name = "txtAmInEnd";
-            this.txtAmInEnd.Size = new System.Drawing.Size(164, 26);
-            this.txtAmInEnd.TabIndex = 2;
-            // 
-            // txtAmInStart
-            // 
-            this.txtAmInStart.Location = new System.Drawing.Point(45, 63);
-            this.txtAmInStart.Name = "txtAmInStart";
-            this.txtAmInStart.Size = new System.Drawing.Size(164, 26);
-            this.txtAmInStart.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(78, 36);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 20);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Time Start";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 109);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Time End";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.groupBox2.Controls.Add(this.txtPmInEnd);
-            this.groupBox2.Controls.Add(this.txtPmInStart);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(714, 492);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(260, 184);
-            this.groupBox2.TabIndex = 7;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Time In PM";
-            // 
-            // txtPmInEnd
-            // 
-            this.txtPmInEnd.Location = new System.Drawing.Point(48, 138);
-            this.txtPmInEnd.Name = "txtPmInEnd";
-            this.txtPmInEnd.Size = new System.Drawing.Size(164, 26);
-            this.txtPmInEnd.TabIndex = 6;
-            // 
-            // txtPmInStart
-            // 
-            this.txtPmInStart.Location = new System.Drawing.Point(48, 64);
-            this.txtPmInStart.Name = "txtPmInStart";
-            this.txtPmInStart.Size = new System.Drawing.Size(164, 26);
-            this.txtPmInStart.TabIndex = 5;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(81, 37);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 20);
-            this.label5.TabIndex = 3;
-            this.label5.Text = "Time Start";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(87, 110);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(76, 20);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Time End";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.groupBox3.Controls.Add(this.txtAmOutEnd);
-            this.groupBox3.Controls.Add(this.txtAmOutStart);
-            this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Controls.Add(this.label4);
-            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(428, 492);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(260, 184);
-            this.groupBox3.TabIndex = 7;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Time Out AM";
-            // 
-            // txtAmOutEnd
-            // 
-            this.txtAmOutEnd.Location = new System.Drawing.Point(48, 138);
-            this.txtAmOutEnd.Name = "txtAmOutEnd";
-            this.txtAmOutEnd.Size = new System.Drawing.Size(164, 26);
-            this.txtAmOutEnd.TabIndex = 6;
-            // 
-            // txtAmOutStart
-            // 
-            this.txtAmOutStart.Location = new System.Drawing.Point(48, 64);
-            this.txtAmOutStart.Name = "txtAmOutStart";
-            this.txtAmOutStart.Size = new System.Drawing.Size(164, 26);
-            this.txtAmOutStart.TabIndex = 5;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(81, 37);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Time Start";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(87, 110);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 20);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Time End";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.groupBox4.Controls.Add(this.txtPmOutEnd);
-            this.groupBox4.Controls.Add(this.txtPmOutStart);
-            this.groupBox4.Controls.Add(this.label7);
-            this.groupBox4.Controls.Add(this.label8);
-            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox4.Location = new System.Drawing.Point(1004, 492);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(260, 184);
-            this.groupBox4.TabIndex = 7;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Time Out PM";
-            // 
-            // txtPmOutEnd
-            // 
-            this.txtPmOutEnd.Location = new System.Drawing.Point(48, 138);
-            this.txtPmOutEnd.Name = "txtPmOutEnd";
-            this.txtPmOutEnd.Size = new System.Drawing.Size(164, 26);
-            this.txtPmOutEnd.TabIndex = 6;
-            // 
-            // txtPmOutStart
-            // 
-            this.txtPmOutStart.Location = new System.Drawing.Point(48, 64);
-            this.txtPmOutStart.Name = "txtPmOutStart";
-            this.txtPmOutStart.Size = new System.Drawing.Size(164, 26);
-            this.txtPmOutStart.TabIndex = 5;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(81, 37);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(82, 20);
-            this.label7.TabIndex = 3;
-            this.label7.Text = "Time Start";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(87, 110);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(76, 20);
-            this.label8.TabIndex = 4;
-            this.label8.Text = "Time End";
-            // 
-            // btnSubmit
-            // 
-            this.btnSubmit.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmit.Location = new System.Drawing.Point(596, 709);
-            this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(222, 63);
-            this.btnSubmit.TabIndex = 16;
-            this.btnSubmit.Text = "SUBMIT";
-            this.btnSubmit.UseVisualStyleBackColor = true;
-            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // panel1
             // 
@@ -406,25 +181,12 @@
             this.ClientSize = new System.Drawing.Size(1406, 819);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.btnSubmit);
-            this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.lbTime);
             this.Controls.Add(this.lbDate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "formToday";
             this.Text = "formToday";
             this.Load += new System.EventHandler(this.formToday_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
-            this.groupBox4.ResumeLayout(false);
-            this.groupBox4.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
@@ -438,27 +200,6 @@
         private System.Windows.Forms.Label lbDate;
         private System.Windows.Forms.Label lbTime;
         private System.Windows.Forms.Timer timerTime;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtAmInEnd;
-        private System.Windows.Forms.TextBox txtAmInStart;
-        private System.Windows.Forms.TextBox txtPmInEnd;
-        private System.Windows.Forms.TextBox txtPmInStart;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtAmOutEnd;
-        private System.Windows.Forms.TextBox txtAmOutStart;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtPmOutEnd;
-        private System.Windows.Forms.TextBox txtPmOutStart;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnExportAttendace;
